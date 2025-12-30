0a325cfa0a9c93c91e7cff3990b950d42759d370
crowderlane001 <122763205+crowderlane001@users.noreply.github.com>
2025-12-29 19:09:09 -0500

    Fifth try

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: '/crystals-cleaning-painting',
-  assetPrefix: '/crystals-cleaning-painting/',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
