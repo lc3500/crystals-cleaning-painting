afdb28337ca2574b4ddfecc74a66d9f5d4185dad
crowderlane001 <122763205+crowderlane001@users.noreply.github.com>
2025-12-29 19:02:31 -0500

    Third try

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: '/crystals-cleaning-painting',
+  assetPrefix: '/crystals-cleaning-painting/',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   }
+  
 };
 
 export default nextConfig;
