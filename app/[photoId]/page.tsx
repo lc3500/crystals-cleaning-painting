29ebc974b209a9eed4c950a9b4b6f420027e4bf6
crowderlane001 <122763205+crowderlane001@users.noreply.github.com>
2025-12-30 23:23:39 -0500

    Image adjustments

--- app/[photoId]/page.tsx
+++ app/[photoId]/page.tsx
@@ -28,7 +28,7 @@ export default async function PhotoDetailPage({
             </header>
             <section className="flex-1 flex items-center justify-center p-4 overflow-hidden">
                 <Image
-                    src={`/crystals-cleaning-pics/${photoId}.webp`}
+                    src={`/${photoId}.webp`}
                     alt={`Photo ${photoId}`}
                     className="rounded-lg shadow-lg max-h-full max-w-full"
                     width={800}
